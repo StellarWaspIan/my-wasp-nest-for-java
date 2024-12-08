b904646077c36e196e853c09caa53aea2a3bae1d
StellarWasp-ian <avilaian2officialac@gmail.com>
2024-12-07 22:06:09 -0300

    nt

--- basics/prom_num.java
+++ basics/prom_num.java
@@ -1,22 +1,23 @@
+
 import java.util.Scanner;
 
 public class prom_num {
-    
+
     static void prom_num() {
-        int cant = 0;
-        double suma_num=0;
-        double prom=0;
+        int cant;
+        double suma_num = 0;
+        double prom;
         Scanner leer_obj = new Scanner(System.in);
 
         System.out.println("Ingrese la cantidad de numeros a promediar:");
         cant = leer_obj.nextInt();
-        
-        for (int i=0;i<cant;i++) {
-            System.out.println("Ingrese el numero "+i+" a promediar:");
+
+        for (int i = 0; i < cant; i++) {
+            System.out.println("Ingrese el numero " + i + " a promediar:");
             suma_num = leer_obj.nextInt() + suma_num;
         }
-        prom=suma_num/cant;
-        System.out.println("El promedio de los "+cant+" numeros ingresados es: "+prom);
+        prom = suma_num / cant;
+        System.out.println("El promedio de los " + cant + " numeros ingresados es: " + prom);
     }
 
     public static void main(String[] args) {
