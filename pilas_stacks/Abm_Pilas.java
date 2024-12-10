423ea74d4a2b33eb6298ddc8596ec3034a205ebc
StellarWasp-ian <avilaian2officialac@gmail.com>
2024-12-10 16:20:50 -0300

    //a

--- pilas_stacks/Abm_Pilas.java
+++ pilas_stacks/Abm_Pilas.java
@@ -8,7 +8,6 @@ public class Abm_Pilas {
     int edad;
     int opcion = 0;
     int repetir = 0;
-    int tamaño;
 
     Scanner leer = new Scanner(System.in);
     Stack<String> nombres = new Stack<>();
@@ -18,17 +17,20 @@ public class Abm_Pilas {
         opcion = leer.nextInt();
         do {
             if (opcion == 1) {
+
                 System.out.println("Ingrese el nombre del alumno: ");
                 nombre = leer.nextLine();
                 nombres.push(nombre);
 
-                System.out.println("Ingrese la edad del alumno: ");
+                while (!leer.hasNextInt()) { // Validar entrada
+                    System.out.println("Ingrese la edad del Alumno: ");
+                    leer.next();
+                }
                 edad = leer.nextInt();
                 edades.push(edad);
 
                 System.out.println("Desea cargar otro alumno? 1(Si), 0 (No)");
                 repetir = leer.nextInt();
-                tamaño = tamaño + 1;
             }
         } while (repetir == 1);
 
@@ -53,18 +55,19 @@ public class Abm_Pilas {
     }
 
     public static void main(String[] args) {
-        System.out.println("Bienvenido al sistema de gestion de alumnos ¿Que desea hacer?");
-        System.out.println("1: Cargar un Alumno");
-        System.out.println("2: Buscar un Alumno");
-        System.out.println("3: Eliminar un Alumno");
-        System.out.println("4: Mostrar a todos los Alumnos");
+        do {
+            System.out.println("Bienvenido al sistema de gestion de alumnos ¿Que desea hacer?");
+            System.out.println("1: Cargar un Alumno");
+            System.out.println("2: Buscar un Alumno");
+            System.out.println("3: Eliminar un Alumno");
+            System.out.println("4: Mostrar a todos los Alumnos");
 
-        Abm_Pilas Obj = new Abm_Pilas(); // Create an object of Main
-        Obj.CargaAlum(); // Call the public method
-        Obj.MostrarAlum();
-        Obj.EliminarAlum();
-        Obj.MostrarPilas();
-
+            Abm_Pilas Obj = new Abm_Pilas();
+            Obj.CargaAlum();
+            Obj.MostrarAlum();
+            Obj.EliminarAlum();
+            Obj.MostrarPilas();
+        } while (repetir == 0);
     }
 
 }
